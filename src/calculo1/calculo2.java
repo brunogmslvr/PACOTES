fb00dcf4e58fef1aee85c59f37c79d291403a3a7
901196 <901196@PITLAB2178770.senac.educ>
2024-02-19 11:32:12 -0300

    PACOTES 19/02

---
+++ src/calculo1/calculo2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculo1;
+
+/**
+ *
+ * @author 901196
+ */
+public class calculo2 {
+        public static int calculo3(int a, int b, int c){
+             int o;
+             o = a + b + c;
+             return o;
+
+}
+    
+}
---
+++ src/resultado1/resultado2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package resultado1;
+
+/**
+ *
+ * @author 901196
+ */
+public interface resultado2 {
+    
+      public static String resultado2.(int );
+    
+    
+}
